54508b57a9f0a20be2062efa95c26bda31d2e87a
Miguel <nov12164@uvg.edu.gt>
2015-11-03 12:19:42 -0600

    Inicio de Proyecto
    Inicio de Recepción de Datos de Raspberry

---
+++ LAGO-UVG/src/ReceptorRaspberry.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Anai
+ */
+public class ReceptorRaspberry {
+    
+    /** Se pretende recibir los datos provenientes de la FPGA mediante un módulo
+     * WIFI de una raspberry pi con arquitectura específica. Esos datos luego de recibirlos se
+     * Parsearán y filtrarán adecuadamente para poder obtener un documento de utilidad.  * 
+     * 
+     */
+    
+}
